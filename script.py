361c0628585fff2e40d448e851e5d6f4fc6b4dbe
jbrofficial <85146090+jbrofficial@users.noreply.github.com>
2021-06-07 11:06:06 +0530

    Update script.py

--- script.py
+++ script.py
@@ -3,7 +3,7 @@ class script(object):
 
     START_MSG = """ <b>Hi {}
 
-You can call this as an Auto Filter Bot if you like :D
+DOWNLOAD MOVIE ON CHANNAL CREATED @JBROFFICIAL🌹 :D
 
 Bot simply search for the files from provided channel according to given query and gives link to those files as buttons!
 
@@ -26,17 +26,16 @@ No need add each filter again!
 Bot will automatically search for your file and give links to that!
 </i>
 
-<b>© @TroJanzHEX</b>"""
+<b>© @cinema_collection_c"""
 
 
     ABOUT_MSG = """⭕️<b>My Name : Auto Filter Bot</b>
     
-⭕️<b>Creater :</b> @TroJanzHEX
+⭕️<b>Creater :</b> @JBROFFICIAL
 
 ⭕️<b>Language :</b> <code>Python3</code>
 
 ⭕️<b>Library :</b> <a href='https://docs.pyrogram.org/'>Pyrogram 1.0.7</a> 
 
-⭕️<b>Tutorial Video :</b> <a href='https://youtu.be/KQVYQAOsFYY'>Video Link</a> 
-
+⭕️<b>Tutorial Video :</b> <a https://github.com/jbrofficial/filterv2
 """
